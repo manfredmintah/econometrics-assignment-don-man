86643d509e2b1c0ecc32d4b4f7d48a65c61ec66b
manfredmintah <152804441+manfredmintah@users.noreply.github.com>
2023-12-03 18:28:32 +0100

    Add files via upload

---
+++ simulation_study_heteroskedasticity.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec  1 21:36:10 2023
+
+@author: manfr
+"""
+
+import numpy as np
+import numpy.linalg as la
+# for ploting
+import matplotlib.pyplot as plt
+# for econometric modells (OLS)
+import statsmodels.api as sm
+# for quantiles
+import scipy.stats as stats
+
+#first birthday
+bd_1 = 
+#second birthday
+bd_2 = 
+group_seed = bd_1 ∗ bd_2
+#seed for the random generator
+rng = np. random . default_rng ( group_seed )
